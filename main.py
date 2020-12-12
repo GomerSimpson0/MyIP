64602a8c674743ca815967b87c703ba9f1750f35
kirill <godynov007@gmail.com>
2020-12-13 00:07:45 +0300

    FullFinalCommit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pymyip 
 import socket
 
+
+print(socket.gethostbyname(socket.gethostname()))
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print(s.getsockname()[0])
