fde4d4844bb271a9738410d564b9d9ea191c4513
kirill <godynov007@gmail.com>
2020-12-20 11:02:57 +0300

    Some changes

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import sys
 import telnetlib
 
 localhostIP = socket.gethostbyname(socket.gethostname())
-print("localhost   -  " + localhostIP)
+print("localhost   -  " + localhostIP + '\n')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 localIP = s.getsockname()[0]
-print("local ip    -  " + localIP)
-print("\n")
+print("local ip    -  " + localIP + '\n')
 
 externalIP = pymyip.get_ip()
-print('external ip -  ' + externalIP)
+print('external ip -  ' + externalIP + '\n')
 print('external open ports: ', end  = '')
 HOST = externalIP
 for i in range(1,1000):
